26a222b7ce010dc65aaede3a086a60df8d0420c4
mdarlow <66172619+mdarlow@users.noreply.github.com>
2020-06-18 12:48:47 -0700

    Push Basic JavaScript Projects folder.
    Initial push of Basic JavaScript Projects folder.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,25 @@
+alert("Hello, World! ");                            //Display box to write string
+
+document.write("document.write test. ");            //Write string
+
+var A = "This is a string. ";                       //Define variable as string
+document.write(A);                                  //Write variable
+window.alert(A);                                    //Display box to write variable
+
+document.write("\"Hello,\" said Michael. ")         //Write string w/ quotes
+
+document.write("Hello. "                            //write concatenated string
++"Hello. "
++"Hello. ")
+
+var B = "Concatenated " + "String. "                //Define variable as concatenated string
+document.write(B)                                   //Write variable
+
+var Me = "Michael", You = "not me";                 //Define multiple variables as strings
+var Me = Me.fontcolor("blue");                      //Assign color to variable
+var You = You.fontcolor("green");                   //Assign color to another variable
+document.write("You are " + You                     //Write string concatenated with two variables
++ " and I am " + Me + ". ");
+
+document.write(2*5);                                //Write expression
+
